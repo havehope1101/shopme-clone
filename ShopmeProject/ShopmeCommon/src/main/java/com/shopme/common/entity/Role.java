2666efba56e30c29a7aef86170efbcad20fed127
havehope1101 <1976836@gmail.com>
2023-02-23 23:05:15 +0700

    code user listing, new user

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -1,6 +1,8 @@
 package com.shopme.common.entity;
 
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,6 +30,10 @@ public class Role {
 	
 	public Role() {
 	}
+	
+	public Role(Integer id) {
+		this.id = id;
+	}
 
 	public Integer getId() {
 		return id;
@@ -53,4 +59,32 @@ public class Role {
 		this.description = description;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+	
 }
---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -0,0 +1,89 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateNewUserWithOneRole() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User userLongLe = new User("haha@gmail.com", "long2020", "Long", "Le");
+		userLongLe.addRole(null);
+		
+		User saveUser = repo.save(userLongLe);
+		assertThat(saveUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateNewUserWithTwoRoles() {
+		User userTank = new User("tank@gmail.com", "tank222", "Tank", "Fight");
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+		
+		userTank.addRole(roleEditor);
+		userTank.addRole(roleAssistant);
+		
+		User saveUser = repo.save(userTank);
+		
+		assertThat(saveUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user -> System.out.println(user));
+	}
+	
+	@Test
+	public void testGetUserById() {
+		User userTank = repo.findById(1).get();
+		System.out.println(userTank);
+		assertThat(userTank).isNotNull();
+	}
+	
+	@Test
+	public void testUpdateUserDetails() {
+		User userTank = repo.findById(1).get();
+		userTank.setEnabled(true);
+		userTank.setEmail("hihihoho@gmail.com");
+		
+		repo.save(userTank);
+	}
+	
+	@Test 
+	public void testUpdateUserRoles() {
+		User userTank = repo.findById(2).get();
+		Role roleEditor = new Role(4);
+		Role roleSalesperson = new Role(3);
+		
+		userTank.getRoles().remove(roleEditor);
+		userTank.addRole(roleSalesperson);
+		
+		repo.save(userTank);
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		Integer userId = 2;
+		repo.deleteById(userId);
+	}
+}
