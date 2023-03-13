9c7025ff2a8aa31c7deb71d489d9c6499ce1b271
havehope1101 <1976836@gmail.com>
2023-03-13 10:11:18 +0700

    CRUD Brand

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -2,18 +2,20 @@ package com.shopme.admin.brand;
 
 import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 
-public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
-
-	Brand save(Brand apple);
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer>, CrudRepository<Brand, Integer> {
 
 	Iterable<Brand> findAll();
 
 	Optional<Brand> findById(int i);
 
 	void deleteById(Integer id);
+	
+	public Long countById(Integer id);
 
 }
