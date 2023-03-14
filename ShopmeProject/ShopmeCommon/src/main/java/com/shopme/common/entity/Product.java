2af8c1a1db3e174f4ee3b4e552002d1c83bc000a
havehope1101 <1976836@gmail.com>
2023-03-14 10:37:39 +0700

    Upload Product Image

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -1,7 +1,10 @@
 package com.shopme.common.entity;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +12,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
@@ -53,6 +58,9 @@ public class Product {
 	private float width;
 	private float height;
 	private float weight;
+	
+	@Column(name = "main_image", nullable = false)
+	private String mainImage;
 
 	@ManyToOne
 	@JoinColumn(name = "category_id")
@@ -61,6 +69,9 @@ public class Product {
 	@ManyToOne
 	@JoinColumn(name = "brand_id")	
 	private Brand brand;
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private Set<ProductImage> images = new HashSet<>();
 
 	public Integer getId() {
 		return id;
@@ -209,5 +220,32 @@ public class Product {
 	@Override
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + "]";
+	}
+	
+
+	public String getMainImage() {
+		return mainImage;
+	}
+
+	public void setMainImage(String mainImage) {
+		this.mainImage = mainImage;
+	}
+
+	public Set<ProductImage> getImages() {
+		return images;
+	}
+
+	public void setImages(Set<ProductImage> images) {
+		this.images = images;
+	}
+	public void addExtraImage(String imageName) {
+		this.images.add(new ProductImage(imageName, this));
+	}
+	
+	@Transient
+	public String getMainImagePath() {
+		if (id == null || mainImage == null) return "/images/image-thumbnail.png";
+
+		return "/product-images/" + this.id + "/" + this.mainImage;
 	}
 }
