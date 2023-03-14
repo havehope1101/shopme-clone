17bc6380405e6bff87227bc553314e31341c6459
havehope1101 <1976836@gmail.com>
2023-03-14 08:56:12 +0700

    Enable Status

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.shopme.common.entity.Product;
 
 @Service
+@Transactional
 public class ProductService {
 
 	@Autowired private ProductRepository repo;
@@ -48,4 +50,9 @@ public class ProductService {
 
 		return "OK";
 	}
+	
+	public void updateProductEnabledStatus(Integer id, boolean enabled) {
+		repo.updateEnabledStatus(id, enabled);
+	}
+
 }
